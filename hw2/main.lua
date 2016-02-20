612a9a258593e9f4cfcf1372e5679b86ef6dff78
Alex Pine <pinesol@pinesol-macbookpro.roam.corp.google.com>
2016-02-20 15:55:30 -0500

    refined debugging mechanism

--- hw2/main.lua
+++ hw2/main.lua
@@ -25,38 +25,61 @@ end
 
 -- creates the 'results_dir' directory if it doesn't exist, and creates the current
 -- experiment's directory, and returns its name.
-function prepare_environment(opt)
-   paths.mkdir(opt.results_dir)  -- TODO what happens if the dir already exists
-
+function prepare_environment(results_dir, exp_name)
+   paths.mkdir(results_dir)
    local full_exp_name = ''
-   if opt.exp_name ~= '' then
-      full_exp_name = opt.exp_name
+   if exp_name ~= '' then
+      full_exp_name = exp_name
    else
       full_exp_name = 'exp'
    end
    full_exp_name = full_exp_name..'_'..os.date("%m%d%H%M%S")
-   local experiment_dir = paths.concat(opt.results_dir, full_exp_name)
+   local experiment_dir = paths.concat(results_dir, full_exp_name)
    print('Saving results at '..experiment_dir)
    paths.mkdir(experiment_dir)
    return experiment_dir
 end
 
--- Creates the debug log file, and sets it to be the default output for
--- io.write() calls.
-function create_debug_log(debug_log_filename, experiment_dir)
+-- Creates the global debug log file, DEBUG_FILE.
+-- LOG(message) can be called once this has been called
+function CREATE_DEBUG_LOG(debug_log_filename, experiment_dir)
    -- set file for io.write calls. 'a' is for 'append' mode.
-   debug_file = io.open(paths.concat(experiment_dir, debug_log_filename), 'a')
-   io.output(debug_file)
-   return debug_file
+   local filepath = paths.concat(experiment_dir, debug_log_filename)
+   -- global variable
+   DEBUG_FILE = assert(io.open(filepath, 'a'))
 end
 
--- Use io.write('blah') to write debug output
+-- Uses io.write to log the given message on a single line.
+-- Assumes CREATE_DEBUG_LOG has already been called.
+function LOG(message)
+   if DEBUG_FILE ~= nil then
+      DEBUG_FILE:write(message..'\n')
+   else
+      print('[ERROR] DEBUG_FILE global var is nil')
+   end
+end
+
+-- saves input options to options.log
+function save_input_options(opt, experiment_dir)
+   local options_str = 'Options\n'
+   for k,v in pairs(opt) do
+      options_str = options_str..k..': '..v..'\n'
+   end
+   local options_filename = paths.concat(experiment_dir, 'options.log')
+   LOG('Writing options to '..options_filename)
+   local options_file = assert(io.open(options_filename, 'a'))
+   options_file:write(options_str)
+   options_file:close()
+end
+
+-- Use LOG('blah') to write debug output
 -- save files in experiment_dir
 function main()
    opt = parse_cmdline()
-   experiment_dir = prepare_environment(opt)
-   debug_file = create_debug_log(opt.debug_log_filename, experiment_dir)
-   io.close(debug_file)
+   experiment_dir = prepare_environment(opt.results_dir, opt.exp_name)
+   CREATE_DEBUG_LOG(opt.debug_log_filename, experiment_dir)
+   save_input_options(opt, experiment_dir)
+   print('Experiment complete.')
 end
 
 main()
