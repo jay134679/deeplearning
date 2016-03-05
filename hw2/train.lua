3767c07f03157ccc0aebb4ed8d246d55ab5b2a41
Alex Pine <pinesol@pinesol-macbookpro.roam.corp.google.com>
2016-03-04 19:59:12 -0500

    pseudo stuff

--- hw2/train.lua
+++ hw2/train.lua
@@ -187,45 +187,162 @@ function train_validate_max_epochs(opt, provider, model,
 end
 
 
--- Construct a new one of these every mini batch, because you need a new unlabledMask for each one.
-local PseudoLabelCriterion, Criterion = torch.class('nn.PseudoLabelCriterion', 'nn.Criterion')
+---- Pseudo Label stuff ----
 
--- crit: Criterion object
--- unlabeledMask: a n x k tensor, where n is the mini-batch size, and k is the number of classes.
---                It has zeros for labeled data, and ones for unlabled data.
--- kAnneal: a scalar we use to weight the unlabled components of the gradient of the loss 
-function PseudoLabelCriterion:__init(crit, unlabeledMask, kAnneal)
-   Criterion.__init(self)
-   self.crit = crit
-   self.unlabeledMask = unlabeledMask
-   self.kAnneal = kAnneal
+
+-- TODO extra data 
+-- TODO unlabeled proportion
+function pseudo_train_one_epoch(opt, trainData, extraData, unlabeledProportion,
+				optimState, model, criterion)
+   model:training()
+
+   local confusion = optim.ConfusionMatrix(10)
+   local parameters, gradParameters = model:getParameters()
+
+   -- labeled data
+   
+   -- creates a random permutation of numbers 1 though the size of the training data,
+   -- then splits them into batches.
+   local labeled_batch_size = math.floor(opt.batchSize*(1-unlabeledProportion)) 
+   local labeled_indices = torch.randperm(trainData.data:size(1)):long():split(labeled_batch_size)
+      -- remove last element so that all the batches have equal size
+   labeled_indices[#labeled_indices] = nil -- TODO huh?
+
+   local targets = nil
+   if opt.no_cuda then
+      targets = torch.FloatTensor(labeled_batch_size)
+   else
+      targets = torch.CudaTensor(labeled_batch_size)
+   end
+
+   local tic = torch.tic()  -- starts timer
+   
+   for t,v in ipairs(labeled_indices) do
+      xlua.progress(t, #labeld_indices)
+      
+      local inputs = trainData.data:index(1, v)
+      targets:copy(trainData.labels:index(1, v))
+      
+      local feval = function(x)
+	 if x ~= parameters then parameters:copy(x) end
+	 gradParameters:zero() -- TODO what does this do?
+	 
+	 local outputs = model:forward(inputs)
+	 local error = criterion:forward(outputs, targets)
+	 local error_gradient = criterion:backward(outputs, targets)
+	 
+	 model:backward(inputs, error_gradient)
+	 
+	 confusion:batchAdd(outputs, targets)
+	 return error, gradParameters
+      end
+      optim.sgd(feval, parameters, optimState)
+   end
+
+   -- unlabeled data
+   -- If kAnneal == 0, then the gradient update will be zero, so skip it.
+   if kAnneal > 0 then   
+      local unlabeled_batch_size = math.floor(opt.batchSize*(unlabeledProportion))
+      unlabeled_indices[#unlabeled_indices] = nil -- TODO huh?
+      
+      local unlabeled_indices = torch.randperm(extraData.data:size(1)):long():split(unlabeled_batch_size)
+      
+      local targets = nil
+      if opt.no_cuda then
+	 targets = torch.FloatTensor(unlabeled_batch_size)
+      else
+	 targets = torch.CudaTensor(unlabeled_batch_size)
+      end
+      
+      for t,v in ipairs(unlabeled_indices) do
+	 xlua.progress(t, #unlabwled_indices)
+	 
+	 local inputs = extraData.data:index(1, v)
+	 
+	 local feval = function(x)
+	    if x ~= parameters then parameters:copy(x) end
+	    gradParameters:zero() -- TODO huh? Doesn't look like this is used.
+	    
+	    local outputs = model:forward(inputs)
+	    
+	    -- compute the pseudo labels.
+	    local _, predictedLabels = torch.max(outputs, 2)
+	    predictedLabels = predictedLabels:float()
+	    targets:copy(predictedLabels)
+	    
+	    local error = criterion:forward(outputs, targets) * kAnneal
+	     -- TODO I hope this is right...
+	    local error_gradient = criterion:backward(outputs, targets) * kAnneal
+	    model:backward(inputs, error_gradient)
+	    
+	    confusion:batchAdd(outputs, targets)
+	    
+	    return error, gradParameters
+	 end
+	 optim.sgd(feval, parameters, optimState)
+      end
+   end
+      
+   confusion:updateValids()
+   local time_secs = torch.toc(tic)
+   DEBUG(('Train accuracy: %.2f'..' %%\t time: %.2f s'):format(
+	    confusion.totalValid * 100, time_secs))
+   
+   local train_acc = confusion.totalValid * 100
+   return train_acc
 end
 
--- returns/sets the error
--- NOTE: modifies target. we assume target has zeros for the unlabeled data.
-function PseudoLabelCriterion:updateOutput(input, target)
-   -- TODO squeeze gets rid of single dimensions. Do we still need to call this?
-   input = input:squeeze()
-   target = type(target) == 'number' and target or target:squeeze()
-   -- Multiplies the input by the unlabeled mask, and adds those values to the target. TODO test!!!
-   target:add(torch.mul(input, self.unlabeledMask))
-   self.crit:updateOutput(input)
-   self.output = self.crit.output
-   return self.output
+
+-- This modifies model and val_logger.
+-- This returns the percentage of samples that were correctly
+-- classified on the validation set and the average number of
+-- milliseconds per sample required to train the model.
+function pseudo_train_validate_max_epochs(opt, provider, model,
+				   custom_model_layer_index, experiment_dir)
+   local valLogger = optim.Logger(paths.concat(experiment_dir, 'val.log'))
+   valLogger:setNames{'% mean class accuracy (train set)', '% mean class accuracy (val set)'}
+   valLogger.showPlot = false
+
+   print('==> setting criterion')
+   local criterion = nn.CrossEntropyCriterion()
+   if not opt.no_cuda then
+      criterion = criterion:cuda()
+   end
+
+   print('==> configuring optimizer')   
+   local optimState = {
+      learningRate = opt.learningRate,
+      weightDecay = opt.weightDecay,
+      momentum = opt.momentum,
+      learningRateDecay = opt.learningRateDecay,
+   }
+
+   -- New Pseudo label stuff
+   kAnneal = 0
+   unlabeledProportion = provider.extraData.size() / (provider.extraData.size() + provider.trainData.size())
+   
+   local val_percent_acc_last = 0
+   for epoch = 1,opt.max_epoch do
+      local epoch_debug_str = "==> online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ']'
+      DEBUG(epoch_debug_str)
+      -- drop learning rate every "epoch_step" epochs
+      if epoch % opt.epoch_step == 0 then
+	 optimState.learningRate = optimState.learningRate/2
+      end
+      
+      print(provider.trainData)
+      local train_acc = train_one_epoch(opt, provider.trainData, optimState,
+					model, criterion)
+      val_confusion = evaluate_model(provider.valData, model) -- TO DO
+      
+      log_validation_stats(valLogger, model, epoch, train_acc, val_confusion,
+			   optimState, experiment_dir)
+      
+      val_percent_acc = val_confusion.totalValid*100
+      print ("LAST  "..val_percent_acc_last.."  NEW  ".. val_percent_acc)
+      -- TODO shouldn't this save only when it's the best model seen so far, not just the best one since we last checked?
+      maybe_save_model(model:get(custom_model_layer_index), epoch, opt.model_save_freq, experiment_dir, val_percent_acc_last, val_percent_acc)
+      val_percent_acc_last = val_percent_acc
+   end
 end
-
--- returns/sets the gradient
-function PseudoLabelCriterion:updateGradInput(input, target)
-   local size = input:size()
-   input = input:squeeze()
-   target = type(target) == 'number' and target or target:squeeze()
-   self.crit:updateGradInput(input, target)
-   -- TODO figure out how to multiply each of the unlabeld gradient terms by kAnneal
-   return self.gradInput
-end
-
-return nn.PseudoLabelCriterion
-
-
-
 
